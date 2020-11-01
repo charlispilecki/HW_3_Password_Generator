28ff1455af27753ddaae7e259c4c4dd39565afb5
Charli <charlispilecki@gmail.com>
2020-10-31 21:22:52 -0500

    Created more functions and tried to make the code cleaner and more dynamic. Added validation code. Password generator now has prompts for length, type of characters, etc.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,28 +12,71 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// My data
-var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
-,"r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9,0,"!","#","$","%","&","(",'"',"'",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
-
 function generatePassword() {
+
+  // get length from user
+  var lengthOfPass = prompt("How long does your password need to be?");
+  lengthOfPass = parseInt(lengthOfPass)
+  if (lengthOfPass < 8) {
+    alert("That password is too short! Passwords must be at least 8 characters.")
+    return
+  }
+  else if (lengthOfPass > 128) {
+    alert("That password is too long! Passwords must be less than 129 characters.")
+    return
+  }
+
+  // get which characters to use
+  var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"
+    , "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+    "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  var spChar = ["!", "#", "$", "%", "&", "(", '"', "'", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+  var useLowerCase = confirm("Would you like your password to include lower case letters?")
+  var useUpperCase = confirm("Would you like your password to include upper case letters?")
+  var useSpChars = confirm("Would you like your password to include special characters?")
+  var useNumbers = confirm("Would you like your password to include numbers?")
+
+  var characters = [];
+
+  if (useLowerCase) {
+    characters = characters.concat(lowerCaseLetters);
+  }
+  if (useUpperCase) {
+    characters = characters.concat(upperCaseLetters);
+  }
+  if (useSpChars) {
+    characters = characters.concat(spChar);
+  }
+  if (useNumbers) {
+    characters = characters.concat(numbers);
+  }
+  if (useLowerCase === false && useUpperCase === false && useSpChars === false && useNumbers === false){
+    alert("What kind of password do you expect me to come up with if there's no upper or lower case letters, and no numbers or special characters???")
+    return
+  }
+
+  // generate the password
   var realPassword = ""
-  for (i = 0; i < 10; i++){
-    var randomCharacter = generateRandomChar();
+  for (i = 0; i < lengthOfPass; i++) {
+    var randomCharacter = generateRandomChar(characters);
     realPassword = realPassword + randomCharacter
   }
-  
+
   return realPassword
+
 }
 
-function generateRandomChar() {
+
+function generateRandomChar(characters) {
   var randomIndex = Math.floor(Math.random() * characters.length)
-  var randomLetter = characters[randomIndex];
-  return randomLetter;
+  var randomCharacter = characters[randomIndex];
+  return randomCharacter;
 }
+
+
 
 
 
